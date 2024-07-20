050768dce2f1f5e4118bab02f979502d010ed6e2
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2024-07-20 14:17:08 -0500

    CortexArgs paths now PathBufs, deafult_values are function calls

--- CLI/cortex/src/lib.rs
+++ CLI/cortex/src/lib.rs
@@ -1,11 +1,16 @@
-use clap::{Parser, Subcommand};
+use clap::{ArgMatches, Parser, Subcommand};
 use serde::{Serialize, Deserialize};
 use nanoid::{alphabet, nanoid};
+use std::path::{Path, PathBuf};
 
 
-fn rand_id() {
-    let alphabet: [char; 2] = ['0', '1'];
-    nanoid!(8, &alphabet);
+fn rand_id() -> String {
+    let alphabet: [char; 16] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    nanoid!(8, &alphabet)
+}
+
+fn default_path(path: &str) -> PathBuf {
+    PathBuf::from(String::from(path))
 }
 
 #[derive(Debug, Parser, Serialize, Deserialize)]
@@ -14,7 +19,6 @@ pub struct CortexArgs {
 
     #[command(subcommand)]
     pub cmd: CortexCommands,
-
 
     /// The name for or id of the job. Defaults to a random value
     #[arg(default_value = rand_id())]
@@ -25,12 +29,12 @@ pub struct CortexArgs {
     pub redundancy: i32,
 
     /// The path to the dockerfile
-    #[arg(short, long, default_value = ".")]
-    pub container: String,
+    #[arg(short, long, default_value = default_path(".").into_os_string())]
+    pub container: PathBuf,
 
     /// The path to the volume to be mounted into the container. If none is provided, one will be created
     #[arg(short, long)]
-    pub volume: Option<String>,
+    pub volume: Option<PathBuf>,
 
     /// The number of GPUS on the host. A range is also accepted
     #[arg(short, long, default_value = "1")]
@@ -45,8 +49,8 @@ pub struct CortexArgs {
     pub model: String,
 
     /// The path to where you would like the results of the job to be placed
-    #[arg(short, long, default_value = ".")]
-    pub out: String,
+    #[arg(short, long, default_value = default_path(".").into_os_string())]
+    pub out: PathBuf,
 
     /// An upper bound on how long this job should run for
     #[arg(short, long, default_value = "none")]
