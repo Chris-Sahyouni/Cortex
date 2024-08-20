87192f15b79e652867dcb1294b93f74fdaf6fa6b
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2024-08-19 20:53:53 -0500

    switched server to tokio

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,36 +1,51 @@
-use std::{net::{TcpListener, TcpStream}, io::{prelude::*, BufReader}, sync::mpsc::channel};
+use std::{io::{prelude::*}, sync::mpsc::channel, error::Error};
 use cortex::{CortexArgs, CortexCommands, JobState};
-use threadpool::ThreadPool;
+use tokio::{io::{AsyncBufReadExt, BufReader}, net::{TcpListener, TcpStream}};
 
-const SERVER_IP_ADDR: &str = "127.0.0.1:4444";
-const NUM_WORKERS: usize = 2;
+extern crate mongodb;
+use mongodb::{bson::doc, options::ClientOptions, Client};
+
+const SERVER_ADDR: &str = "127.0.0.1:32503";
+const DB_CONN_STRING: &str = "mongodb://localhost:27017";
 
 
-
-fn main() {
-    let listener = TcpListener::bind(SERVER_IP_ADDR).unwrap();
-    let threadpool = ThreadPool::new(NUM_WORKERS);
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind(SERVER_ADDR).await.unwrap();
     // let (sender, receiver) = channel();
 
+    let db_conn = ClientOptions::parse(DB_CONN_STRING).await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
         // connection attempt may not be successful so we pattern match it to ensure its Ok
-        if let Ok(stream) = stream {
-            threadpool.execute(move || {
+        if let Ok((stream, _)) = listener.accept().await {
+            tokio::spawn(async move {
+                // use the result here somehow
                 handle_connection(stream);
             });
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn db_connection() -> Result<Client, Box<dyn Error>> {
+    let client_options = ClientOptions::parse(DB_CONN_STRING).await?;
+    let client = Client::with_options(client_options)?;
+    Ok(client)
+}
+
+
+
+async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+
+
     let mut buf_reader = BufReader::new(&mut stream);
     let mut serialized_args = String::new();
-    buf_reader.read_line(&mut serialized_args).unwrap();
+    buf_reader.read_line(&mut serialized_args).await?;
     let args: CortexArgs = serde_json::from_str(serialized_args.as_str()).unwrap();
 
-    
 
+
+    Ok(())
 }
 
 /* ---------------------------------- Tests --------------------------------- */
